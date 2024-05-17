0f191052d97ab0dcd992c371f0f0f7fc7f79f788
mmjax <maxskgtr@gmail.com>
2024-05-17 16:27:58 +0300

    from sqlite to postgre

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -85,10 +85,38 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv(
+            'DB_ENGINE',
+            default='django.db.backends.postgresql'
+        ),
+        'NAME': os.getenv(
+            'DB_NAME',
+            default='postgres'
+        ),
+        'USER': os.getenv(
+            'POSTGRES_USER',
+            default='postgres'
+        ),
+        'PASSWORD': os.getenv(
+            'POSTGRES_PASSWORD',
+            default='postgres'
+        ),
+        'HOST': os.getenv(
+            'DB_HOST',
+            default='mail-sys-db'
+        ),
+        'PORT': os.getenv(
+            'DB_PORT',
+            default='5432'
+        )
     }
 }
 
